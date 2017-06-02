a15aac84c2ebb31c3b92e2a6570bd3a35b7d8df2
Arseniy Kucher <beeffallo@gmail.com>
2017-06-02 19:34:55 +0300

    Speed testing

--- AlibabaData/AlData.Console/Program.cs
+++ AlibabaData/AlData.Console/Program.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
 
 namespace AlData.C
 {
     class Program
     {
+        private static TimeSpan _totalTime = new TimeSpan();
+        private static int _counter;
+
         static void Main(string[] args)
         {
             DownloadPages();
@@ -16,7 +22,34 @@ namespace AlData.C
 
         private static void DownloadPages()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 57; i++)
+            {
+                DownPage(i);
+            }
+        }
+
+        private static async void DownPage(int i)
+        {
+            Console.WriteLine(i + " started!");
+            var sw = new Stopwatch();
+            sw.Start();
+            //var rq = WebRequest.Create($"https://www.alibaba.com/corporations/agriculture_tyres/--CN------------------50/{i}.html");
+            var rq = WebRequest.Create($"https://www.alibaba.com/corporations/agriculture_tyres/{i}.html");
+            var rp = await rq.GetResponseAsync();
+            sw.Stop();
+            _totalTime = _totalTime.Add(sw.Elapsed);
+            Console.WriteLine(sw.Elapsed);
+
+            //var stream = rp.GetResponseStream();
+            //var str = new StreamReader(stream).ReadToEnd();
+
+            _counter++;
+            Console.WriteLine("Counter: " + _counter);
+            Console.WriteLine($"Page {i} downloaded!");
+
+            //File.WriteAllText($"Test {i}.txt", str);
+
+            if (i == 20) Console.WriteLine("Total time: " + _totalTime);
         }
     }
 }
